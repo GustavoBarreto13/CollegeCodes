d4865e94af98720d784680cddd1818c0fbc1697f
Gustavo Barreto <gustavobarreto1304@gmail.com>
2021-09-30 14:38:42 -0300

    LP1A3 - Lista 1 Ex 1

---
+++ 3º Semestre/LP1A3/Lista De Exercícios/Lista1/src/Ex2.java
@@ -0,0 +1,13 @@
+﻿import java.util.Scanner;
+
+public class Ex2 {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner n = new Scanner(System.in);
+            
+        System.out.println("Digite um número inteiro: ");
+        int num = n.nextInt();
+    
+        
+    }
+}
