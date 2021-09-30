6d3a1cc7712a791b78de72cf3c90db70fdcf0629
Gustavo Barreto <gustavobarreto1304@gmail.com>
2021-09-30 16:36:53 -0300

    LP1A3_Lista1_Ex5

---
+++ 3º Semestre/LP1A3/Lista De Exercícios/Lista1/src/Ex5.java
@@ -0,0 +1,19 @@
+﻿import java.util.Scanner;
+
+public class Ex5 {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner n = new Scanner(System.in);
+            
+        System.out.println("Digite um número inteiro: ");
+        int num = n.nextInt();
+        
+        int sum = 0;
+        for(int i = 0; i < num; i++){
+            sum += (i+1);
+        }
+
+        System.out.println("A soma de todos os número de 1 a " + num + " é: " + sum);
+        n.close();
+    }
+}
