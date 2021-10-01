828d553cb791c61be148e70e593cd87a28f30535
Gustavo Barreto <gustavobarreto1304@gmail.com>
2021-10-01 16:49:20 -0300

    LP1A3_Lista1_Ex11*

---
+++ 3º Semestre/LP1A3/Lista De Exercícios/Lista1/src/Ex11.java
@@ -0,0 +1,28 @@
+﻿import java.util.Scanner;
+
+/* 1. Tendo como dados de entrada a altura e o sexo de uma pessoa, 
+    construa um algoritmo que calcule seu  peso ideal, utilizando as seguintes fórmulas:
+    - Para Homens: (72.7 *h) – 58;
+    - Para Mulheres: (62.1 * h) – 44.7 */
+
+public class Ex11 {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner n = new Scanner(System.in);
+        double pi = 0;
+        
+        System.out.println("Digite o Sexo (M ou F): ");
+        String sexo = n.nextLine();
+
+        System.out.println("Digite a altura em m: ");
+        double num = n.nextDouble();
+
+        if(sexo == "M")
+            pi = ((72.7 * num) - 58.0);
+        
+
+        System.out.println("O peso ideal é: " + pi);
+        n.close();
+        
+    }
+}
