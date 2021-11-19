7e214e823c8bf79ab6039dd01cd53ff4e1d09e73
Gustavo Barreto <gustavobarreto1304@gmail.com>
2021-11-19 18:08:23 -0300

    Começo_projetoLP1A3

---
+++ 3º Semestre/LP1A3/ProjetoPassagens/src/Aeronave.java
@@ -0,0 +1,21 @@
+﻿public class Aeronave {
+    
+    String modelo;
+    
+    // construtor
+    public Aeronave(String modelo)
+    {
+        this.modelo = modelo;
+    }
+
+    // getters
+    public String getModelo() {
+        return modelo;
+    }
+
+    //setters
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    
+}
